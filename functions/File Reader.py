7c21b60e74ee70baffa8ef167c9b39ad3af55e28
Ridwan Mukhtar <rm109@hw.ac.uk>
2019-11-28 21:21:56 +0000

    added json reader functionality and q4 stuff

--- functions/File Reader.py
+++ functions/File Reader.py
@@ -1,16 +1,24 @@
 import json
-
-def reader(url):
-	data = ["test"]
-	with open("issuu_sample.json", "r") as read_file:
-		data = json.load(read_file)
+def json_readr(file):
+	data = []
+	for line in open(file, mode="r"):
+		data.append(json.loads(line))
 	return data
 
 def getCountries(docID):
 	countries = []
+	i = 0
 	for d in data:
-		if (d["subject_doc_id"] == docID):
-			countries.append(d["visitor_country"])
+		try:
+			if (d["env_doc_id"] == docID):
+				countries.append(d["visitor_country"])
+		except:
+			try:
+				if (d["subject_doc_id"] == docID):
+					countries.append(d["visitor_country"])
+			except:
+				i = i+1
+				print(i)
 	return countries
 
 def getBrowsers():
@@ -23,12 +31,47 @@ def documentVisitors(docID):
 	visitors = []
 	for d in data:
 		if (d["subject_doc_id"] == docID):
-			visitors.append(d["visitor_uuid"])
-	dic = {}
-	dic[docID] = visitors
-	return dic
+			if d["visitor_uuid"] not in visitors:
+				visitors.append(d["visitor_uuid"])
+	return visitors
 
-data = reader("issuu_sample.json")
-print(getCountries("140228202800-6ef39a241f35301a9a42cd0ed21e5fb0"))
-print(getBrowsers())
-print(documentVisitors("140228202800-6ef39a241f35301a9a42cd0ed21e5fb0"))
+def visitorDocuments(visID):
+	documents = []
+	for d in data:
+		if (d["visitor_uuid"] == visID):
+			if (d["subject_doc_id"] not in documents):
+				documents.append(d["subject_doc_id"])
+	return documents
+
+def getList(variable, docID=None):
+	list = []
+	for d in data:
+		if docID:
+			if (d["subject_doc_id"] == docID):
+				list.append(d[variable])
+		else:
+			list.append(d[variable])
+	return list
+
+def alsoLike(docID, visID = None):
+	visitors = documentVisitors(docID)
+	documents = []
+	for d in visitors:
+		documents.append(visitorDocuments(d))
+	return documents
+	
+
+data = json_readr("sample_100k_lines.json")
+#print(getList("visitor_country", "140228202800-6ef39a241f35301a9a42cd0ed21e5fb0"))
+getCountries("140228202800-6ef39a241f35301a9a42cd0ed21e5fb0")
+#print()
+#print(getList("visitor_useragent"))
+#print(getBrowsers())
+#print()
+#print(getList("visitor_uuid","140228202800-6ef39a241f35301a9a42cd0ed21e5fb0"))
+#print(documentVisitors("140228202800-6ef39a241f35301a9a42cd0ed21e5fb0"))
+#print()
+#print(visitorDocuments("745409913574d4c6"))
+print("worked")
+#print(alsoLike("140228202800-6ef39a241f35301a9a42cd0ed21e5fb0"))
+
