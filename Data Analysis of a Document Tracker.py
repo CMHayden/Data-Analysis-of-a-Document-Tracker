e21e609d55e8607b4489de380ea44c9445faebf7
Ridwan Mukhtar <rm109@hw.ac.uk>
2019-11-28 18:47:53 +0000

    added file reader + getting lists from json

--- Data Analysis of a Document Tracker.py
+++ Data Analysis of a Document Tracker.py
@@ -1,7 +1,24 @@
 import matplotlib.pyplot as plt
+import json
 
-# Function which takes data as a parameter and stores the occurrences of country codes in a dictionary
-data = ["uk", "mx", "uk", "uk", "az", "jp"]
+def reader(url):
+    data = ["test"]
+    with open("datasets/issuu_sample.json", "r") as read_file:
+        data = json.load(read_file)
+    return data
+
+def getCountries(docID):
+    countries = []
+    for d in data:
+        if (d["subject_doc_id"] == docID):
+            countries.append(d["visitor_country"])
+    return countries
+
+def getBrowsers():
+    browsers = []
+    for d in data:
+        browsers.append(d["visitor_useragent"])
+    return browsers
 
 def histogram(dict, xlabel, title):
     plt.xlabel(xlabel, fontsize=15)
@@ -25,4 +42,20 @@ def countriesCounter(data):
             countriesDict[country] = 1
     return countriesDict
 
-histogram(countriesCounter((data)), "Countries", "Counting Countries")
+def browserCounter(data):
+    browsersDict = {}
+    for browser in data:
+		if browser in browsersDict:
+			browsersDict[browser] += 1
+		else:
+			browsersDict[browser] = 1
+    return browsersDict
+
+
+data = reader("issuu_sample.json")
+
+countries = getCountries("140228202800-6ef39a241f35301a9a42cd0ed21e5fb0")
+histogram(countriesCounter((countries)), "Countries", "Counting Countries")
+
+browsers = getBrowsers()
+histogram(browserCounter((browsers)), "Browsers", "Counting Browsers")
